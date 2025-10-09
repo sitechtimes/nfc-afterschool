b56d9ae3f5ed47da98cbcebed22610e04188ccee
Benjamin Bernadskiy <78938589+bluepuppy9@users.noreply.github.com>
2025-10-09 08:08:58 -0400

    bruh

--- frontend/app/pages/login.vue
+++ frontend/app/pages/login.vue
@@ -1,18 +1,34 @@
 <template>
-  <div class="flex min-h-screen p-4 items-center justify-center">
-    <div class="card card-xl md:w-3/4 w-4/5 justify-center">
-      <div class="card-body justify-center">
-        <h2 class="card-title justify-center">Login</h2>
-        <div class="w-full">
+  <div class="flex flex-col h-screen p-4 items-center justify-center gap-10">
+    <h1 class="text-3xl font-bold">After School Attendance</h1>
+    <div
+      class="card card-xl md:w-1/2 w-4/5 justify-center shadow-xl hover:md:shadow-2xl hover:md:scale-101 transition duration-700"
+    >
+      <div class="card-body justify-center h-fit">
+        <h2 class="card-title justify-center">Enter account information</h2>
+        <form @submit.prevent class="flex flex-col w-full gap-2">
           <input
             class="input w-full bg-base-200"
             type="text"
+            v-model="form.username"
             placeholder="Enter Username"
           />
-        </div>
+          <input
+            class="input w-full bg-base-200"
+            type="password"
+            v-model="form.passoword"
+            placeholder="Enter Password"
+          />
+          <button class="btn btn-primary btn-block" type="submit">Login</button>
+        </form>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const form = ref({
+  username: "",
+  passoword: "",
+});
+</script>
